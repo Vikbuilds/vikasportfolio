7f726202c5b5f2dc51ad151e623aba192763ffe9
GitPirates <starshivamverma2@gmail.com>
2026-07-25 00:54:26 +0530

    Update AnimatedThemeToggler to set icon size for theme toggle buttons

--- src/components/ui/animated-theme-toggler.tsx
+++ src/components/ui/animated-theme-toggler.tsx
@@ -296,7 +296,7 @@ export const AnimatedThemeToggler = ({
       className={cn(className)}
       {...props}
     >
-      {isDark ? <Sun /> : <Moon />}
+      {isDark ? <Sun size={18} /> : <Moon size={18} />}
       <span className="sr-only">Toggle theme</span>
     </button>
   )
